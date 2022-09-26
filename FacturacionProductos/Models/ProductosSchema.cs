64ce9879d15f912ed949671553b7f099073e0da8
scarletgonzalez <smgonzalez@neural.com.do>
2022-09-26 15:55:44 -0400

    Add project files.

---
+++ FacturacionProductos/Models/ProductosSchema.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace FacturacionProductos.Models
+{
+    public class ProductosSchema
+    {
+        public int Codigo { get; set; }
+        public string Descripcion { get; set; }
+        public bool Estado { get; set; }
+        public int Cantidad { get; set; }
+        public decimal Precio { get; set; }
+        public string fecha  { get; set; }
+
+    }
+
+    public class FacturacionDbContext : ApplicationDbContext
+    {   public DbSet<ProductosSchema> Productos { get; set; }
+
+
+    }
+    
+
+
+
+
+}
