270422bb5de4dbf7dee99c385df2df0783f833d0
scarletgonzalez <smgonzalez@neural.com.do>
2022-09-27 11:57:29 -0400

    Setting up

--- FacturacionProductos/Models/ProductosSchema.cs
+++ FacturacionProductos/Models/ProductosSchema.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
 namespace FacturacionProductos.Models
 {
-    public class ProductosSchema
+    public class Productos
     {
+        [Key]
         public int Codigo { get; set; }
         public string Descripcion { get; set; }
-        public bool Estado { get; set; }
+        public bool Activo { get; set; }
         public int Cantidad { get; set; }
         public decimal Precio { get; set; }
         public string fecha  { get; set; }
@@ -18,7 +20,8 @@ namespace FacturacionProductos.Models
     }
 
     public class FacturacionDbContext : ApplicationDbContext
-    {   public DbSet<ProductosSchema> Productos { get; set; }
+    {   
+        public DbSet<Productos> Producto { get; set; }
 
 
     }
